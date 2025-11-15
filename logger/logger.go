14ad3d0cfe1a04a5353368a8234d7815a16b139f
Ulysses Souza <ulyssessouza@gmail.com>
2025-11-15 02:42:06 +0100

    lint: Migrate golangci-lint and fix linter issues
    Signed-off-by: Ulysses Souza <ulyssessouza@gmail.com>

--- logger/logger.go
+++ logger/logger.go
@@ -31,7 +31,7 @@ func (l Level) String() string {
 	return "UNKNOWN"
 }
 
-// Logger is a custom logger with levels
+// Logger is a custom logger with levels.
 type Logger struct {
 	m     sync.Mutex
 	level Level
@@ -52,7 +52,7 @@ func New(out io.Writer, level Level) *Logger {
 // DefaultLogger is the default logger instance.
 var DefaultLogger = New(os.Stderr, INFO)
 
-// SetOutput sets the output writer for the default logger
+// SetOutput sets the output writer for the default logger.
 func SetOutput(out io.Writer) {
 	DefaultLogger.m.Lock()
 	defer DefaultLogger.m.Unlock()
@@ -60,55 +60,55 @@ func SetOutput(out io.Writer) {
 	DefaultLogger.std.SetOutput(out)
 }
 
-// SetLevel sets the log level for the default logger
+// SetLevel sets the log level for the default logger.
 func SetLevel(level Level) {
 	DefaultLogger.m.Lock()
 	defer DefaultLogger.m.Unlock()
 	DefaultLogger.level = level
 }
 
-// Debugf logs a debug message
+// Debugf logs a debug .
 func Debugf(format string, v ...interface{}) {
 	DefaultLogger.Debugf(format, v...)
 }
 
-// Infof logs an info message
+// Infof logs an info message.
 func Infof(format string, v ...interface{}) {
 	DefaultLogger.Infof(format, v...)
 }
 
-// Warnf logs a warning message
+// Warnf logs a warning message.
 func Warnf(format string, v ...interface{}) {
 	DefaultLogger.Warnf(format, v...)
 }
 
-// Errorf logs an error message
+// Errorf logs an error message.
 func Errorf(format string, v ...interface{}) {
 	DefaultLogger.Errorf(format, v...)
 }
 
-// Debugf logs a debug message
+// Debugf logs a debug message.
 func (l *Logger) Debugf(format string, v ...interface{}) {
 	if l.level <= DEBUG {
 		l.std.Printf("[DEBUG] "+format, v...)
 	}
 }
 
-// Infof logs an info message
+// Infof logs an info message.
 func (l *Logger) Infof(format string, v ...interface{}) {
 	if l.level <= INFO {
 		l.std.Printf("[INFO] "+format, v...)
 	}
 }
 
-// Warnf logs a warning message
+// Warnf logs a warning message.
 func (l *Logger) Warnf(format string, v ...interface{}) {
 	if l.level <= WARN {
 		l.std.Printf("[WARN] "+format, v...)
 	}
 }
 
-// Errorf logs an error message
+// Errorf logs an error message.
 func (l *Logger) Errorf(format string, v ...interface{}) {
 	if l.level <= ERROR {
 		l.std.Printf("[ERROR] "+format, v...)
